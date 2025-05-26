b69aec40fe36e0a8f9f68922556f83cf726c1ae6
AayushiGK <aayushik.2512@gmail.com>
2025-05-26 13:51:01 +0530

    substring

--- stringOps/contains-substring.js
+++ stringOps/contains-substring.js
@@ -4,6 +4,8 @@ const superStr_2 = "Python";
 const subStr = "asc";
 
 const match = superStr_1.match(subStr);
+// 2nd Way indexOf()
+// const match = superStr_1.indexOf(subStr);
 console.log(
   match !== null ? `match found at index ${match.index}` : `No Match found`
 );
